58973910d06864965279ccdd692e46a82f44cb68
Burri <mahipal.reddy.burri@accenture.com>
2018-06-12 16:48:45 +0200

    first commit

---
+++ src/main/java/com/mahi/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.mahi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home()
+	{
+		return "Welcome to Accenture Germany";
+	}
+}
